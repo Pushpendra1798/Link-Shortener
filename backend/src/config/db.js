fb18877a4235f79c87c9eda5560db19ac63ef17a
Pushpendra Shah <pushpendra7725@gmail.com>
2026-09-13 19:40:18 +0530

    fix MONGO_URI

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ import config from "./config.js";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(config.MONGO_URL);
+        await mongoose.connect(config.MONGO_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
